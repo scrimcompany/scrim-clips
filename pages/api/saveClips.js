48d2901e537042bc1ececae47db4390c5c22f409
Adrian Lee <jun.irok@gmail.com>
2020-10-25 21:02:48 -0700

    Add generatedFor property for matches to incidate demo processed for that player

--- pages/api/saveClips.js
+++ pages/api/saveClips.js
@@ -4,10 +4,17 @@ const saveClips = async (req, res) => {
     const matchToken = req.query.matchToken;
     const steamId = req.query.steamId;
     const json = req.body;
-    const byRound = json[steamId];
+    let data = json[steamId];
+
+    // if (Object.keys(byRound).length === 0) {
+    //     data = { empty: true };
+    // }
 
     try {
         await admin.firestore().collection("matches").doc(matchToken).collection("clips").doc(steamId).set(byRound);
+        await admin.firestore().collection("matches").doc(matchToken).set({
+            generatedFor: admin.firestore.FieldValue.arrayUnion(steamId)
+        }, { merge: true })
         return res.status(200).json("OK")
     } catch (error) {
         console.log(error);
