fc7c9f8be2722b5c845727dfb08d9cc9a8562edb
Adrian Lee <jun.irok@gmail.com>
2020-10-26 18:29:13 -0700

    Hide manual add match button

--- pages/index.js
+++ pages/index.js
@@ -1,16 +1,9 @@
 import NextLink from 'next/link'
 import { useUser } from '../utils/auth/useUser'
-import { Button, Center, VStack, Wrap, Heading, } from "@chakra-ui/core"
+import { Button, Center, VStack, Wrap, Heading } from "@chakra-ui/core"
 import MyMatches from './../components/MyMatches';
 import MyAPISecrets from './../components/MyAPISecrets';
 import AddMatchToken from './../components/AddMatchToken';
-
-const fetcher = (url, token) =>
-  fetch(url, {
-    method: 'GET',
-    headers: new Headers({ 'Content-Type': 'application/json', token }),
-    credentials: 'same-origin',
-  }).then((res) => res.json())
 
 const Index = () => {
   const { user, logout } = useUser()
@@ -34,7 +27,7 @@ const Index = () => {
         <Center>
           <Wrap p={4}>
             <MyAPISecrets />
-            <AddMatchToken />
+            {/* <AddMatchToken /> */}
 
             <Button
               onClick={() => logout()}
